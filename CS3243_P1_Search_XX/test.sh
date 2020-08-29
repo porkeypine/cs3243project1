8bcf3d108b30298f71b0aecc542345dfc91a3cdc
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2020-08-29 16:21:37 +0800

    Repackaged All files into the folders required

---
+++ CS3243_P1_Search_XX/test.sh
@@ -0,0 +1,9 @@
+#TEST
+#python pacman.py -l tinyMaze -p SearchAgent -a fn=tinyMazeSearch
+#DFS
+python pacman.py -l tinyMaze -p SearchAgent
+#python pacman.py -l mediumMaze -p SearchAgent
+#python pacman.py -l bigMaze -z .5 -p SearchAgent
+#BFS
+#python pacman.py -l mediumMaze -p SearchAgent -a fn=bfs
+#python pacman.py -l bigMaze -p SearchAgent -a fn=bfs -z .5
